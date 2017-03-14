4c274b09266824bd2d9aceac40e5a86e7133d7bb
krichdev <kgrichardson2@gmail.com>
2017-03-14 13:32:28 -0700

    added click events to move items to different lists

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
-/* setup your angular app here */
+var app = angular.module("FruitsAndVeggies", []);
 
-// debug stuff to show the app is loading and fruit / veggies are available
-console.log('App Started');
-console.log('Fruit count', fruits.length);
-console.log('Veggie count', vegetables.length);
+app.controller("ListCtrl", ["$scope", function($scope){
+  var combined = fruits.concat(vegetables);
+  $scope.fullList = combined.sort(function(a, b){return 0.5 - Math.random()});
+  $scope.newFruits = [];
+  $scope.newVeggies = [];
+
+  $scope.moveLeft = function(){
+    $scope.fullList.splice(this.$index, 1);
+    $scope.newFruits.push(this.item);
+
+    if(fruits.includes(this.item)){
+      console.log(this.item, "is a fruit")
+      $scope.class = "correct";
+    } else {
+      console.log(this.item, "is not a fruit")
+    }
+  }
+
+  $scope.moveRight = function(){
+    $scope.fullList.splice(this.$index, 1);
+    $scope.newVeggies.push(this.item);
+
+    if(vegetables.includes(this.item)){
+      console.log(this.item, "is a vegetable")
+    } else {
+      console.log(this.item, "is not a vegetable")
+    }
+  }
+
+  $scope.combineRight = function(){
+    $scope.newFruits.splice(this.$index, 1);
+    $scope.fullList.push(this.item);
+  }
+
+  $scope.combineLeft = function(){
+    $scope.newVeggies.splice(this.$index, 1);
+    $scope.fullList.push(this.item);
+  }
+
+}]);
